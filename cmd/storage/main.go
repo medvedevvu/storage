d2b30b06e0834a90a137f914faa10942348c74dc
medvedevvu <medvedevvu72@gmail.com>
2022-06-20 23:30:56 +0300

    It is done

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -15,5 +15,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("It uploaded", file)
+	restoredFile, err := st.GetByID(file.ID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("It restored", restoredFile)
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,18 +1,37 @@
 package storage
 
-import "github.com/medvedevvu/storage/internal/file"
+import (
+	"fmt"
 
-type Storage struct{}
+	"github.com/google/uuid"
+	"github.com/medvedevvu/storage/internal/file"
+)
+
+type Storage struct {
+	Files map[uuid.UUID]*file.File
+}
 
 func NewStorage() *Storage {
-	return &Storage{}
+	return &Storage{
+		Files: make(map[uuid.UUID]*file.File),
+	}
 }
 
 func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
-	return file.NewFile(filename, blob)
-	//newFile, err := file.NewFile(filename, blob)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// return newFile, nil
+	newFile, err := file.NewFile(filename, blob)
+	if err != nil {
+		return nil, err
+	}
+
+	s.Files[newFile.ID] = newFile
+
+	return newFile, nil
+}
+
+func (s *Storage) GetByID(fileID uuid.UUID) (*file.File, error) {
+	foundFile, ok := s.Files[fileID]
+	if !ok {
+		return nil, fmt.Errorf("file %v not found", fileID)
+	}
+	return foundFile, nil
 }
